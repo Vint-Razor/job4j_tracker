521ff13bb9ce8cce6985142054b77d1f6a7262d3
Vint_Razor <vint-razor@yandex.ru>
2021-09-14 23:05:57 +0500

    2.2. Рефакторинг - Площадь треугольника. [#122642 #202708]

--- src/test/java/ru/job4j/oop/TriangleTest.java
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -3,6 +3,7 @@ package ru.job4j.oop;
 import static org.hamcrest.Matchers.closeTo;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TriangleTest {
@@ -15,5 +16,28 @@ public class TriangleTest {
         Triangle triangle = new Triangle(a, b, c);
         double rsl = triangle.area();
         assertThat(rsl, closeTo(8, 0.001));
+    }
+
+    @Test
+    public void whenAbMoreBcCaThenMinus1() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 4);
+        Point c = new Point(0, 2);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        assertThat(rsl, closeTo(-1, 0.001));
+    }
+
+    @Test
+    public void whenAbMoreBcCaThenFalse() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        Point c = new Point(0, 1);
+        Triangle abc = new Triangle(a, b, c);
+        double ab = a.distance(b);
+        double ac = a.distance(c);
+        double bc = b.distance(c);
+        boolean rsl = abc.exist(ab, ac, bc);
+        Assert.assertFalse(rsl);
     }
 }
