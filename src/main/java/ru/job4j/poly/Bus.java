7b9ea04ca2f81cdce846884a41e61ebd4d0d7981
Vint_Razor <vint-razor@yandex.ru>
2021-09-22 23:11:45 +0500

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void numPassengers(byte passengers) {
+
+    }
+
+    @Override
+    public float gasUp(byte gas) {
+        return 0;
+    }
+}
