0ef5d9c30b5132afe9b7dbbe3f4245d4d9fb4b90
Vint_Razor <vint-razor@yandex.ru>
2021-10-15 21:14:33 +0500

    2.1. Тест на ValidateInput [#311887]

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class ValidateInputTest {
+
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu: ");
+        assertThat(selected, is(1));
+
+    }
+
+    @Test
+    public void whenValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"2"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu: ");
+        assertThat(selected, is(2));
+    }
+
+    @Test
+    public void whenMultipleIncorrectInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1", "2", "3"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu: ");
+        String ln = System.lineSeparator();
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenNegativeNumberInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"-1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu: ");
+        String ln = System.lineSeparator();
+        assertThat(selected, is(-1));
+    }
+}
