57b1cc375c84bf7a69a4ab0bace101e0abc5cf11
Рустам <vint-razor@yandex.ru>
2021-10-25 09:03:12 +0500

    3. Checkstyle с Maven [#257606 #220061]

--- src/main/java/ru/job4j/strategy/Paint.java
+++ src/main/java/ru/job4j/strategy/Paint.java
@@ -1,13 +1,13 @@
 package ru.job4j.strategy;
 
 public class Paint {
-    public void draw(Shape shape) {
+    public void drawShape(Shape shape) {
         System.out.println(shape.draw());
     }
 
     public static void main(String[] args) {
         Paint paint = new Paint();
-        paint.draw(new Triangle());
-        paint.draw(new Square());
+        paint.drawShape(new Triangle());
+        paint.drawShape(new Square());
     }
 }
