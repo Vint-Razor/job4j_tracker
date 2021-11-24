30637a36b2c519a1b63521dd10cdb0f58077c9ab
Рустам <vint-razor@yandex.ru>
2021-11-24 09:28:40 +0500

    2. Отсортировать департаменты [#34352]

--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -1,9 +1,6 @@
 package ru.job4j.collection;
 
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Departments {
 
@@ -23,10 +20,10 @@ public class Departments {
     }
 
     public static void sortAsc(List<String> orgs) {
-
+        orgs.sort(Comparator.naturalOrder());
     }
 
     public static void sortDesc(List<String> orgs) {
-
+        orgs.sort(new DepDescComp());
     }
 }
--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -25,4 +25,12 @@ public class DepartmentsTest {
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
      }
+
+    @Test
+    public void whenThreeDep() {
+        List<String> input = Arrays.asList("k1/sk1/ssk1");
+        List<String> expect = Arrays.asList("k1", "k1/sk1", "k1/sk1/ssk1");
+        List<String> result = Departments.fillGaps(input);
+        assertThat(result, is(expect));
+    }
 }
