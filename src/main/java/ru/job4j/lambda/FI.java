48890d062d154a9ada30ed1bad7d5b12931443a4
Vint_Razor <vint-razor@yandex.ru>
2021-11-27 22:53:10 +0500

    2.1. Лямбда [#58165 #233501]

---
+++ src/main/java/ru/job4j/lambda/FI.java
@@ -0,0 +1,26 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class FI {
+    public static void main(String[] args) {
+        Attachment[] atts = {
+                new Attachment("image 1", 20),
+                new Attachment("image 2", 120),
+                new Attachment("image 3", 23)
+        };
+        String[] strArr = {
+                "zabc",
+                "xcdef",
+                "ydsert"
+        };
+        Comparator<Attachment> comparator = (left, right) -> left.getSize() - right.getSize();
+        Comparator<String> cmpSize = (left, right) -> left.length() - right.length();
+        Comparator<String> cmpText = (left, right) -> left.compareTo(right);
+        Comparator<String> cmpDescSize = (left, right) -> right.length() - left.length();
+        Arrays.sort(atts, comparator);
+        Arrays.sort(strArr, cmpDescSize);
+        System.out.println(Arrays.toString(strArr));
+    }
+}
