5958b5d219fff3c19fa5aa64991667c87d487011
Рустам <vint-razor@yandex.ru>
2021-12-01 10:55:55 +0500

    0. Stream API [#58166 #234475]

--- src/main/java/ru/job4j/stream/Filter.java
+++ src/main/java/ru/job4j/stream/Filter.java
@@ -8,7 +8,7 @@ public class Filter {
     public static void main(String[] args) {
         List<Integer> nums = Arrays.asList(123, 32, -287, 3, -9, 0);
         List<Integer> rsl = nums.stream()
-                .filter(num -> num >= 0)
+                .filter(num -> num > 0)
                 .collect(Collectors.toList());
         rsl.forEach(System.out::println);
     }
