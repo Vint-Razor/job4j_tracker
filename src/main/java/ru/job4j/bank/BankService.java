1f0681cc072f1a1ec1a6ee4d5fc0ad3db8f37e07
Vint_Razor <vint-razor@yandex.ru>
2021-12-04 17:04:25 +0500

    6. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#24260]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -18,6 +18,7 @@ public class BankService {
     /**
      * Метод добавляет нового пользователя и создаёт новый
      * ArrayList с аккаунтами
+     *
      * @param user - информация о пользователе
      */
     public void addUser(User user) {
@@ -27,8 +28,9 @@ public class BankService {
     /**
      * Метод добавляет новый аккаунт к существующему пользователю,
      * на вход принимает номер паспорта и новый аккаунт
+     *
      * @param passport - номер паспорта
-     * @param account - название аккаунта
+     * @param account  - название аккаунта
      */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
@@ -42,44 +44,46 @@ public class BankService {
 
     /**
      * Метод ищет пользователя по номеру паспорта
+     *
      * @param passport - номер паспорта
      * @return возвращает пользователя, если не находит
      * то null
      */
     public User findByPassport(String passport) {
-        for (User user : users.keySet()) {
-            if (user.getPassport().equals(passport)) {
-                return user;
-            }
-        }
-        return null;
+        return users.keySet()
+                .stream()
+                .filter(s -> s.getPassport().equals(passport))
+                .findFirst()
+                .orElse(null);
     }
 
     /**
      * Метод ищет аккаунт по номеру паспорта и реквизитам
-     * @param passport - номер паспорта
+     *
+     * @param passport  - номер паспорта
      * @param requisite - номер реквизита
      * @return возвращает аккаунт, если не находит то null
      */
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
         if (user != null) {
-            for (Account account : users.get(user)) {
-                if (requisite.equals(account.getRequisite())) {
-                    return account;
-                }
-            }
+            return users.get(user)
+                    .stream()
+                    .filter(s -> s.getRequisite().equals(requisite))
+                    .findFirst()
+                    .orElse(null);
         }
         return null;
     }
 
     /**
      * Метод осуществляет денежный перевод между двумя счетами
-     * @param srcPassport номер паспорта отправителя
-     * @param srcRequisite номер реквизита отправителя
-     * @param destPassport номер паспорта получателя
+     *
+     * @param srcPassport   номер паспорта отправителя
+     * @param srcRequisite  номер реквизита отправителя
+     * @param destPassport  номер паспорта получателя
      * @param destRequisite номер реквизита получателя
-     * @param amount - сумма перевода
+     * @param amount        - сумма перевода
      * @return true - если перевод успешен, иначе false
      */
     public boolean transferMoney(String srcPassport, String srcRequisite,
