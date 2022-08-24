89a802fbedce20444e22fd8bb8907c890ec07cb5
Vint_Razor <vint-razor@yandex.ru>
2022-08-24 06:10:16 +0500

    2. Трекер SQL [#1734] - дописал все методы. Заявки из БД читает, но не записывает.

--- src/main/java/ru/job4j/tracker/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -3,6 +3,7 @@ package ru.job4j.tracker;
 import java.io.InputStream;
 import java.sql.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
@@ -52,38 +53,93 @@ public class SqlTracker implements Store, AutoCloseable {
 
     @Override
     public boolean replace(int id, Item item) {
-        return false;
+        boolean rsl = false;
+        try (PreparedStatement statement = cn.prepareStatement(
+                "UPDATE items SET name = ?, created = ? WHERE id = ?;")) {
+            statement.setString(1, item.getName());
+            statement.setTimestamp(2, Timestamp.valueOf(item.getCreated()));
+            statement.setInt(3, id);
+            rsl = statement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rsl;
     }
 
     @Override
     public boolean deleted(int id) {
-        return false;
+        boolean rsl = false;
+        try (PreparedStatement statement = cn.prepareStatement(
+                "DELETE FROM items WHERE id = ?;")) {
+            statement.setInt(1, id);
+            rsl = statement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rsl;
     }
 
     @Override
     public List<Item> findAll() {
-        return null;
+        List<Item> list = new ArrayList<>();
+        try (PreparedStatement statement = cn.prepareStatement(
+                "SELECT * FROM items;")) {
+            list = saveToList(statement);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 
     @Override
     public List<Item> findByName(String key) {
-        return null;
+        List<Item> list = new ArrayList<>();
+        try (PreparedStatement statement = cn.prepareStatement(
+                "SELECT * FROM items WHERE name = ?;")) {
+            statement.setString(1, key);
+            list = saveToList(statement);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 
     @Override
     public Item findById(int id) {
-        return null;
-    }
-
-    private void edit(String sql) {
-        try (Statement statement = cn.createStatement()) {
-            statement.execute(sql);
+        Item item = new Item();
+        try (PreparedStatement statement = cn.prepareStatement(
+                "SELECT * FROM items WHERE id = ?;")) {
+            statement.setInt(1, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                item = new Item(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        timeConverter(resultSet.getTimestamp("created"))
+                );
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return item;
     }
 
-    private LocalDateTime timeConverter(Timestamp time) {
+    private List<Item> saveToList(PreparedStatement statement) {
+        List<Item> list = new ArrayList<>();
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    list.add(new Item(
+                            resultSet.getInt("id"),
+                            resultSet.getString("name"),
+                            timeConverter(resultSet.getTimestamp("created"))
+                    ));
+                }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    private static LocalDateTime timeConverter(Timestamp time) {
         long millis = System.currentTimeMillis();
         Timestamp timestamp = new Timestamp(millis);
         return timestamp.toLocalDateTime();
