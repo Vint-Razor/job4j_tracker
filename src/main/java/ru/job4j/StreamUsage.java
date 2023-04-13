c2e708a9678337227947d43c5c567f22c6c1dc76
Vint-Razor <vint-razor@yandex.ru>
2023-04-13 21:48:54 +0500

    изменил имя пользователя и аккаунт

--- src/main/java/ru/job4j/StreamUsage.java
+++ src/main/java/ru/job4j/StreamUsage.java
@@ -16,7 +16,7 @@ public class StreamUsage {
     public static void main(String[] args) {
         List<Task> tasks = List.of(
                 new Task("Bug #1", 10),
-                new Task("Task #2", 20),
+                new Task("Task #2", 30),
                 new Task("Bug #3", 40)
         );
         tasks.stream()
